e5b04e2fb7b1268c346e0861ee2483e7fb87de70
veerlev <veerle.verkeerd@gmail.com>
2019-06-25 22:29:59 +0300

    Create ErrorBoundry

--- robotfriends/src/components/CardList.js
+++ robotfriends/src/components/CardList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
+	if(true){throw new Error('Oepsie!');}
 	return (
 		<div>
 			 {robots.map((user, i) =>{
